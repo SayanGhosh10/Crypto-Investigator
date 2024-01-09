b5b601b041dcb2d84eddff022d3f240df6c171ca
SayanGhosh10 <Sayanmyself50@gmail.com>
2024-01-09 17:38:08 +0530

    UI updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Banner from './componenets/Banner';
+import Footer from './componenets/Footer';
 
 function App() {
   const [inputData, setInputData] = useState('');
@@ -11,34 +13,38 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-        const response = await axios.post('https://crypto-investigator-backend.onrender.com/process_input', {
-            input_data: inputData,
-        });
+      const response = await axios.post('https://crypto-investigator-backend.onrender.com/process_input', {
+        input_data: inputData,
+      });
 
-        setProcessedData(response.data.processed_data);
+      setProcessedData(response.data.processed_data);
     } catch (error) {
-        console.log('Error processing input:', error);
+      console.log('Error processing input:', error);
     }
-};
+  };
 
 
   return (
-    <div className="App container d-flex align-items-center justify-content-center my-5">
-      <div>
-        <h1>Crypto Investigator</h1>
-        <label className='form-label'>
-          Input Data:
-          <input className='form-control form-control-lg' type="text" value={inputData} onChange={handleInputChange} />
-        </label>
-        <button className='mx-3 btn btn-warning' onClick={handleSubmit}>Process Input</button>
-        {processedData && (
-          <div>
-            <h2>Result</h2>
-            <p>{processedData}</p>
-          </div>
-        )}
+    <>
+      <Banner />
+      <div className="App container d-flex align-items-center justify-content-center my-5">
+        <div>
+          <h1>Crypto Investigator</h1>
+          <label className='form-label'>
+            Input Data:
+            <input className='form-control form-control-lg' type="text" value={inputData} onChange={handleInputChange} />
+          </label>
+          <button className='mx-3 btn btn-warning' onClick={handleSubmit}>Process Input</button>
+          {processedData && (
+            <div>
+              <h2>Result</h2>
+              <p>{processedData}</p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
